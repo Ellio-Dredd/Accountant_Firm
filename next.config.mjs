465fe220cfcf1f74b9cd2f03a1b4e3ed021a87be
Yasas Arandara <143877390+Ellio-Dredd@users.noreply.github.com>
2025-10-17 01:08:42 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,14 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
+  // Prepend /E-learning to all routes in production
   basePath: isProd ? "/E-learning" : "",
-  assetPrefix: isProd ? "/E-learning/" : "",
-  output: "export",
+  // Prepend /E-learning to all static assets
+  assetPrefix: isProd ? "/E-learning" : "",
+  output: "export", // full static export
   images: {
-    unoptimized: true,
+    unoptimized: true, // disables Next.js image optimization (required for static export)
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
