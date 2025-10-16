ef38883e8f79af9840a3f1ab0f732b7b2d38c139
Yasas Arandara <143877390+Ellio-Dredd@users.noreply.github.com>
2025-10-17 01:35:15 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  basePath: isProd ? "/E-learning" : "",
-  assetPrefix: isProd ? "/E-learning" : "",
+  basePath: "",
+  assetPrefix: "",
   output: "export",
   images: {
     unoptimized: true,
