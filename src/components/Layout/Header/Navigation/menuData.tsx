8eaad7ae81358cd94eae67750c78e8960ccda3d6
= <=>
2025-10-18 16:46:20 +0530

    my commit

--- src/components/Layout/Header/Navigation/menuData.tsx
+++ src/components/Layout/Header/Navigation/menuData.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export const headerData: HeaderItem[] = [
   { label: "Home", href: "/" },
-  { label: "Mentor", href: "/#mentor" },
+  { label: "About Us", href: "/#mentor" },
   { label: "Blog", href: "/blog" },
-  { label: "Testimonial", href: "/#testimonial" },
+  { label: "Tax Calculetor", href: "/#testimonial" },
   { label: "Contact", href: "/#contactus" },
   // { label: "Services", href: "/#services" },
   { label: "Services", href: "/Services" }, 
