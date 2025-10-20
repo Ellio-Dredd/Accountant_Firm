a2b7a68bcb07188def54740efad0627b467ed8cb
Yasas Arandara <yasaschamikara123@gmail.com>
2025-10-20 18:24:36 +0530

    ContactUs navigation fixed

--- src/components/Layout/Header/Navigation/menuData.tsx
+++ src/components/Layout/Header/Navigation/menuData.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export const headerData: HeaderItem[] = [
   { label: "Home", href: "/" },
   { label: "Blog", href: "/blog" },
-  { label: "Tax Calculetor", href: "/#testimonial" },
-  { label: "Contact", href: "/#contactus" },
+  { label: "Tax Calculator", href: "/#testimonial" },
+  { label: "Contact", href: "/ContactUs" },
   // { label: "Services", href: "/#services" },
   { label: "Services", href: "/Services" },
   { label: "Team", href: "/team"} 
